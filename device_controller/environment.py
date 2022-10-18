750da17fa73e817d1e3f8fa14023bbd577b89ca9
Daniel Watrous <daniel.watrous@gmail.com>
2022-10-18 18:07:23 -0500

    add weather observations

--- device_controller/environment.py
+++ device_controller/environment.py
@@ -5,6 +5,7 @@ from astral import LocationInfo
 from astral.sun import sun
 from astral.geocoder import database, lookup
 from noaa_sdk import NOAA
+import json
 
 def get_sundata(city):
     # must be in https://astral.readthedocs.io/en/latest/index.html#cities
@@ -21,10 +22,17 @@ def get_sundata(city):
     
 def get_hourly_weather_forcast_24hr(zip):
     n = NOAA()
-    res = n.get_forecasts(zip, 'US', hourly=True)
-    return res[0:24]
+    forecast = n.get_forecasts(zip, 'US', hourly=True)
+    return forecast[0:24]
+
+def get_hourly_weather_observations_24hr(zip):
+    n = NOAA()
+    observations = n.get_observations(zip, 'US')
+    return observations
 
 if __name__ == '__main__':
     testcity = "Houston"
     print(get_sundata(testcity))
     print(get_hourly_weather_forcast_24hr('77070'))
+    for observation in get_hourly_weather_observations_24hr('77070'):
+        print(json.dumps(observation, indent=2))
