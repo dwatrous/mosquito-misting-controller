f7e8d3cd93d3b78d9020ba08392e98b3f5605068
Daniel Watrous <daniel.watrous@gmail.com>
2022-10-27 19:58:35 -0500

    make environment a class

--- device_controller/environment.py
+++ device_controller/environment.py
@@ -6,33 +6,61 @@ from astral.sun import sun
 from astral.geocoder import database, lookup
 from noaa_sdk import NOAA
 import json
+import constants
 
-def get_sundata(city):
-    # must be in https://astral.readthedocs.io/en/latest/index.html#cities
-    location = lookup(city, database())
-    s = sun(location.observer, date=datetime.date.today(), tzinfo=location.timezone)
-    sundata = {
-        "dawn": s["dawn"],
-        "sunrise": s["sunrise"],
-        "noon": s["noon"],
-        "sunset": s["sunset"],
-        "dusk": s["dusk"]
-    }
-    return sundata
+class environment:
+
+    def __init__(self, zip=None, city=None) -> None:
+        if zip == None:
+            self.zip = constants.default_zip
+        if city == None:
+            self.city = constants.default_environment_city
+
+        self.noaaapi = NOAA()
+
+        # generate and fetch
+        self.weather_forecast_24hr = self.get_hourly_weather_forcast_24hr()
+        self.weather_observations_24hr = self.get_hourly_weather_observations_24hr()
+        self.sundata = self.get_sundata()
+
+    def get_forecast_24hr(self):
+        return self.weather_forecast_24hr
+
+    def get_observations_24hr(self):
+        return self.weather_observations_24hr
     
-def get_hourly_weather_forcast_24hr(zip):
-    n = NOAA()
-    forecast = n.get_forecasts(zip, 'US', hourly=True)
-    return forecast[0:24]
+    def get_sundata(self):
+        return self.sundata
 
-def get_hourly_weather_observations_24hr(zip):
-    n = NOAA()
-    observations = n.get_observations(zip, 'US')
-    return observations
+    # generate and fetch data
+    def get_sundata(self):
+        # must be in https://astral.readthedocs.io/en/latest/index.html#cities
+        location = lookup(self.city, database())
+        s = sun(location.observer, date=datetime.date.today(), tzinfo=location.timezone)
+        sundata = {
+            "dawn": s["dawn"],
+            "sunrise": s["sunrise"],
+            "noon": s["noon"],
+            "sunset": s["sunset"],
+            "dusk": s["dusk"]
+        }
+        return sundata
+        
+    def get_hourly_weather_forcast_24hr(self):
+        forecast = self.noaaapi.get_forecasts(self.zip, 'US', hourly=True)
+        return forecast[0:24]
+
+    def get_hourly_weather_observations_24hr(self):
+        observations = self.noaaapi.get_observations(self.zip, 'US')
+        return observations
+    
+    # calculations
+    def get_rain_next_24hr(self):
+        pass
 
 if __name__ == '__main__':
-    testcity = "Houston"
-    print(get_sundata(testcity))
-    print(get_hourly_weather_forcast_24hr('77070'))
-    for observation in get_hourly_weather_observations_24hr('77070'):
+    env = environment()
+    print(env.get_sundata())
+    print(env.get_forecast_24hr())
+    for observation in env.get_observations_24hr():
         print(json.dumps(observation, indent=2))
