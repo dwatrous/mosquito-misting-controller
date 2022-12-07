140a20cdde6bb70ec12a1060d611c98a3e733dac
Daniel Watrous <daniel.watrous@gmail.com>
2022-12-06 19:53:03 -0600

    update test to start with new default time and exit after 3 minutes

--- device_controller/device_test.py
+++ device_controller/device_test.py
@@ -18,11 +18,14 @@ assert len(mydevice.zones) == 1
 mydevice_zone0 = mydevice.zones[0]
 assert mydevice_zone0.calculate_valve_openings() == zone_test.expected_default_timing
 
-trigger_now = datetime.datetime.now().replace(hour=5, minute=59, second=3, microsecond=0)
+trigger_now = datetime.datetime.now().replace(hour=22, minute=59, second=37, microsecond=0)
+test_duration = trigger_now
 
 if __name__ == '__main__':
     with freeze_time(trigger_now, tick=True):
-        mydevice.schedule_sprays()
+        scheduler = mydevice.schedule_sprays()
         while True:
             print("waiting 1 minute at ", datetime.datetime.now())
             sleep(5)
+            if datetime.datetime.now() - test_duration > datetime.timedelta(minutes=3): break
+        scheduler.set()
