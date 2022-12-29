58dd4888c0680c0fa2cb7eff7db27d19b67a8081
Daniel Watrous <daniel.watrous@gmail.com>
2022-12-29 16:43:54 -0600

    begin working controller

--- device_controller/controller.py
+++ device_controller/controller.py
@@ -3,16 +3,62 @@
 
 # This is the main controller file
 
-import constants
-import jsonpickle
-import device
+import json
 from os.path import exists
+from device import device
 
-# https://jsonpickle.github.io/
-# load from disk
-# save to disk
-# fetch from server
-# save to server
+import firebase_admin
+from firebase_admin import firestore
+from pathlib import Path
+
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+# load in configuration
+configfile = Path(__file__).with_name("config.json")
+with configfile.open("r") as configreader:
+    config = json.loads(configreader.read())
+
+# handle first time setup
+# start wifi hotspot
+# start server to accept connection from the app
+# write initial account configuration file
+
+# Connect to cloud to check for existing device configuration
+account_ref = db.collection(u"accounts").document(config["account_id"])
+account_doc = account_ref.get()
+if account_doc.exists:
+    account = account_doc.to_dict()
+else:
+    print(u"No such account!") # TODO handle error
+# check for empty devices and create default config
+if account["devices"] == {}:
+    new_default_device = device()
+    account["devices"][config["device_name"]] = new_default_device.get_devicedefinition()
+    account_ref.set(account)
+    new_default_device.schedule_thread_kill_signal.set()
+    del new_default_device
+# load device configuration if available (cloud first, then local)
+try:
+    device_config = account["devices"][config["device_name"]]
+except:
+    print(u"No such device!") # TODO handle error
+deviceconfigfile = Path(__file__).with_name("deviceconfig.json")
+with deviceconfigfile.open("w") as configwriter:
+    configwriter.write(json.dumps(device_config))
+# if WiFi unavailable, do ???
+
+
+# create the device instance
+
+# while True
+# listen for messages from the cloud
+# handle message
+# MESSAGES: reload configuration, spray now, skip next spray, get device status
+# check and send device status (hourly?) (may include error state that will be handled from the cloud)
+
+# periodically refresh schedule (daily should be fine)
 
 # with open(constants.configfilename, "w") as text_file:
 #     print(frozen, file=text_file)
