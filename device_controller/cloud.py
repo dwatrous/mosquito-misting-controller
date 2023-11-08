c60299398c31721463eb80429d83692ff91dd5e2
Daniel Watrous <daniel.watrous@gmail.com>
2023-11-07 21:42:56 -0600

    implement messaging and debug

--- device_controller/cloud.py
+++ device_controller/cloud.py
@@ -1,15 +1,16 @@
 # Import Firebase REST API 
 import atexit
 import firebase
-import requests
-import utils
+from utils import Config, app_log
 import datetime
-import sys
 
 class Cloud(object):
     # get Config
-    config = utils.Config()
+    config = Config()
     authenticated_device_account = None
+    device_details = None
+    device_details_reload = False
+    message_processor = None
 
     # Firebase app https://github.com/AsifArmanRahman/firebase-rest-api/tree/main
     app = None
@@ -41,27 +42,30 @@ class Cloud(object):
     def get_authenticated_device_account(self):
         # handle initial authentication
         if self.authenticated_device_account == None:
-            user = self.auth.sign_in_with_email_and_password(self.config.device_email, self.config.device_password)
+            device = self.auth.sign_in_with_email_and_password(self.config.device_email, self.config.device_password)
             self.authenticated_device_account = {
-                "uid": user["localId"],
-                "serial_number": user["displayName"],
-                "serial_number_as_email": user["email"],
-                "idToken": user["idToken"],
-                "refreshToken": user["refreshToken"],
-                "expiresAt": user["expiresAt"]
+                "uid": device["localId"],
+                "serial_number": device["displayName"],
+                "serial_number_as_email": device["email"],
+                "idToken": device["idToken"],
+                "refreshToken": device["refreshToken"],
+                "expiresAt": device["expiresAt"]
             }
         # handle refresh
         if datetime.datetime.now() > datetime.datetime.fromtimestamp(self.authenticated_device_account["expiresAt"]):
-            user = self.auth.refresh(self.authenticated_device_account['refreshToken'])
-            self.authenticated_device_account["idToken"] = user["idToken"]
-            self.authenticated_device_account["expiresAt"] = user["expiresAt"]
+            device = self.auth.refresh(self.authenticated_device_account['refreshToken'])
+            self.authenticated_device_account["idToken"] = device["idToken"]
+            self.authenticated_device_account["expiresAt"] = device["expiresAt"]
         # finally, return authenticated_device_account, which may just be the cached values
         return self.authenticated_device_account
         # TODO handle error case
 
     # Firestore
     def device_get(self):
-        return self.ds.collection(u'devices').document(self.config.device_email).get(token=self.get_authenticated_device_account()["idToken"])
+        # handle initial authentication
+        if self.device_details == None or self.device_details_reload:
+            self.device_details = self.ds.collection(u'devices').document(self.config.device_email).get(token=self.get_authenticated_device_account()["idToken"])
+        return self.device_details
 
     def device_update(self, device):
         return self.ds.collection(u'devices').document(self.config.device_email).update(device, token=self.get_authenticated_device_account()["idToken"])
@@ -70,47 +74,57 @@ class Cloud(object):
         # TODO this is probably too optimistic, add error handling
         self.ds.collection(u'devices').document(self.config.device_email).collection(u'sprayoccurrences').add(spraydata, token=self.get_authenticated_device_account()["idToken"])
 
-    def account_get(self, account_id):
-        try:
-            account = self.ds.collection(u"Users").document(account_id).get(token=self.get_authenticated_device_account()["idToken"])
-        except requests.exceptions.HTTPError:
-            # TODO cleanup...assume 404, but it could be something else, like a 403, which might be confusing
-            the_type, the_value, the_traceback = sys.exc_info()
-            return the_value
-        return account
-
-    def account_update(self, account_id, account):
-        try:
-            self.ds.collection()(u"Users").document(account_id).update(account, token=self.get_authenticated_device_account()["idToken"])
-        except:
-            pass    # TODO add error handling here
-    
-
     # Firebase realtime database (used for messaging)
-    def _build_message(self, event, info, action, sender):
+    def _build_message(self, event, info, action):
         msg = {
-            "sender": sender,
+            "sender": self.get_authenticated_device_account()["uid"],
+            "recipient": self.device_get()["owner"],
             "message": {
                 "event": event,
-                "action": action,
                 "info": info
             },
             "time": datetime.datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
         }
         return msg
 
-    def send_message(self, event, info="", action=None, sender="device"):
-        message = self._build_message(event, info, action, sender)
-        self.db.child("devices").child(self.get_authenticated_device_account()["uid"]).child("messages").push(message, token=self.get_authenticated_device_account()["idToken"])
+    def send_message(self, event, info="", action=None):
+        message = self._build_message(event, info, action)
+        self.db.child("messages").push(message, token=self.get_authenticated_device_account()["idToken"])
+        app_log.info("Sent message with event: %s" % id)
 
     def listen_for_messages(self, callback):
-        self.my_stream = self.db.child("devices").child(self.get_authenticated_device_account()["uid"]).child("messages").stream(callback, token=self.get_authenticated_device_account()["idToken"])
+        self.message_processor = callback
+        self.my_stream = self.db.child("messages").stream(self.message_capture, token=self.get_authenticated_device_account()["idToken"])
+        app_log.info("Listening for messages")        
         atexit.register(self.my_stream.close)
 
-    def mark_message_read(self, message):
-        message_key = message["path"][1:]
-        self.db.child("devices").child(self.get_authenticated_device_account()["uid"]).child("messages").child(message_key).remove(token=self.get_authenticated_device_account()["idToken"])
-        self.db.child("devices").child(self.get_authenticated_device_account()["uid"]).child("processed").push(message["data"], token=self.get_authenticated_device_account()["idToken"])
+    def archive_message(self, id, message):
+        self.db.child("processed").push(message, token=self.get_authenticated_device_account()["idToken"])
+        self.db.child("messages").child(id).remove(token=self.get_authenticated_device_account()["idToken"])
+        app_log.info("Message %s move to processed" % id)
+
+    def message_capture(self, message):
+        process_outcome = {}
+        # identify empty message and ignore
+        if message["data"] == None:
+            app_log.info("Empty message: %s", message)
+        
+        # identify single message and evaluate
+        elif "message" in message["data"].keys():
+            # if message intended for this device, process
+            if message["data"]["recipient"] == cloud.device_get()["uid"]:
+                if self.message_processor(message["data"]):
+                    self.archive_message(message["path"][1:], message["data"])
+        
+        # identify multiple messages and evaluate
+        else:
+            for key in message["data"].keys():
+                # if message intended for this device, process
+                if message["data"][key]["recipient"] == cloud.device_get()["uid"]:
+                    if self.message_processor(message["data"][key]):
+                        self.archive_message(key, message["data"][key])
+                else:
+                    app_log.debug("Message intended for %s", message["data"][key]["recipient"])
 
 if __name__ == '__main__':
     cloud = Cloud()
@@ -119,8 +133,14 @@ if __name__ == '__main__':
     cloud.write_spray_occurence_ds(spraydata)
     mydevice = cloud.device_get()
     print(mydevice)
-    
-    account = cloud.account_get('t8abTKbDoCY8lf3q6XeRcl2H3fA3')
-    print(account)
-    notaccount = cloud.account_get('fake')
-    print(notaccount)
+
+    # test messages
+    def message_processor(message):
+        app_log.info("Received message: %s", message)
+        # TODO do something with the message
+        return True
+
+    cloud.listen_for_messages(message_processor)
+    cloud.send_message("SPRAY_NOTIFICATION", "Spray started")
+    x = input("Press any key when messaging done")
+    print(x)
