edb4d15eba97134ece4c1b8348c73207359a4a60
Daniel Watrous <daniel.watrous@gmail.com>
2023-12-28 19:47:30 -0600

    add clean option, not implemented. check pid to decide if instance is running.

--- device_controller/mm_controller/cli.py
+++ device_controller/mm_controller/cli.py
@@ -1,49 +1,36 @@
 import argparse
+import os
 import sys
 from importlib.metadata import version
 import subprocess
 from datetime import datetime, timedelta
-import re
+import psutil
 
 # Define the log file path
 # TODO this may get out of sync with config
 log_file = "/home/mm/device.log"
 
-# Regular expression to extract timestamp
-timestamp_regex = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})"
+def is_instance_running():
+    """Checks if another instance of the script is already running."""
+    my_pid = os.getpid()
+    print("my_pid: %s" % my_pid)
+    for proc in psutil.process_iter():
+        if proc.cmdline() == ['/home/mm/.ctrlenv/bin/python', '/home/mm/.ctrlenv/bin/mmctrl', '--start'] and proc.pid != my_pid:
+            return True
+    return False
 
-def service_is_running(minutes_back=2):
-    with open(log_file, "r") as f:
-        # Read the last line
-        last_line = f.readlines()[-1].strip()
-
-        # Extract the timestamp
-        match = re.search(timestamp_regex, last_line)
-        if match:
-            timestamp_str = match.group(1)
-
-            # Parse the timestamp into datetime object
-            try:
-                log_time = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S,%f")
-            except ValueError:
-                print(f"Error parsing timestamp: {timestamp_str}")
-
-    # Get the current time
-    current_time = datetime.now()
-
-    # Check if the log time is within the past 2 minutes
-    time_diff = current_time - log_time
-
-    return time_diff < timedelta(minutes=minutes_back)
-
-def restart_service_if_running():
+def start_service():
     """Restarts the service if it is running."""
-    if service_is_running():
+    if is_instance_running():
         subprocess.run(["/usr/bin/sudo", "/usr/bin/systemctl", "restart", "mmctrl.service"])
 
 def enable_service():
     """Enable and start the service (usually after registration)."""
     subprocess.run(["/usr/bin/sudo", "/usr/bin/systemctl", "enable", "--now", "mmctrl.service"])
+
+def stop_service():
+    """Stop the service"""
+    subprocess.run(["/usr/bin/sudo", "/usr/bin/systemctl", "stop", "mmctrl.service"])
 
 def get_loglevel(loglevel_arg):
     import logging
@@ -66,6 +53,7 @@ def cli():
     parser.add_argument("-d", "--diagnostics", action="store_true", help="Run diagnostics.")
     parser.add_argument("-c", "--calibrate", action="store_true", help="Calibrate connected devices.")
     parser.add_argument("-r", "--register", action="store_true", help=argparse.SUPPRESS) # TODO secure this so consumers can't register devices
+    parser.add_argument("-n", "--clean", action="store_true", help="Remove WiFi and logs from device.")
     parser.add_argument("--validate", choices=["cloud", "weather"], help="Run various exercises to validate connectivity.")
     parser.add_argument("-l", "--loglevel", choices=["DEBUG", "INFO", "WARN", "ERROR"], default="DEBUG", help="Set logging level.")
 
@@ -75,39 +63,51 @@ def cli():
         print(version('mm_controller'))
         sys.exit(0)
     
-    try:
-        if args.calibrate:
+    if args.calibrate:
+        try:
+            stop_service()
             print("Running calibration...")
             from mm_controller import calibrate_device
             calibrate_device.calibrate_all()
-    except KeyboardInterrupt:
-        restart_service_if_running()
+        except KeyboardInterrupt:
+            start_service()
         
+    if args.clean:
+        print("Cleaning device...")
+        # from mm_controller import clean_device
+        # remove WiFi creds
+        # remove or change user password
+        # clean_device.clean()
+        # start_service()
+
     if args.register:
         print("Running registration...")
+        stop_service()
         from mm_controller import register_device
         if not register_device.is_registered():
             register_device.register()
             enable_service()
         else:
             print("Device password exists. Skipping...")
-            restart_service_if_running()
+            start_service()
 
-    try:
-        if args.diagnostics:
+    if args.diagnostics:
+        try:
+            stop_service()
             print("Running diagnostics...")
             from mm_controller import device_sensors
             device_sensors.rundiagnostics()
-            restart_service_if_running()
-    except KeyboardInterrupt:
-        restart_service_if_running()
+            start_service()
+        except KeyboardInterrupt:
+            start_service()
         
     if args.validate == "cloud":
        pass
     elif args.validate == "weather":
        pass
+
     if args.start:        
-        if not service_is_running():
+        if not is_instance_running():
             print("Log level: %s" % args.loglevel)
             from mm_controller.utils import my_handler
             my_handler.setLevel(get_loglevel(args.loglevel))
