28d58b5f6b369998020add1fa5a6fd3c7e228726
Daniel Watrous <daniel.watrous@gmail.com>
2024-01-13 23:20:16 -0600

    untested changes to cli and more granular approach to calibration

--- device_controller/mm_controller/calibrate_device.py
+++ device_controller/mm_controller/calibrate_device.py
@@ -3,25 +3,32 @@ import json
 from mm_controller.device_sensors import calibrate_scale, calibrate_line_in, calibrate_line_out, calibrate_vacuum
 from mm_controller.utils import Config, app_log
 
-def calibrate_all():
+def calibrate(scale=False, line_in=False, line_out=False, vacuum=False):
     config = Config()
 
     # write calibration values to config.json
     with config.configfile.open("r") as configreader:
         configfile_contents = json.loads(configreader.read())
 
-    configfile_contents["device"]["scale_offset"] = calibrate_scale()
-    configfile_contents["device"]["line_in_offset_psi"] = calibrate_line_in()
-    configfile_contents["device"]["line_out_offset_psi"] = calibrate_line_out()
-    configfile_contents["device"]["vacuum_offset_kpa"] = calibrate_vacuum()
+    if scale:
+        configfile_contents["device"]["scale_offset"] = calibrate_scale()
+        app_log.info("Scale Calibrated")
+    if line_in:
+        configfile_contents["device"]["line_in_offset_psi"] = calibrate_line_in()
+        app_log.info("Line In Calibrated")
+    if line_out:
+        configfile_contents["device"]["line_out_offset_psi"] = calibrate_line_out()
+        app_log.info("Line Out Calibrated")
+    if vacuum:
+        configfile_contents["device"]["vacuum_offset_kpa"] = calibrate_vacuum()
+        app_log.info("Vacuum Calibrated")
 
     with config.configfile.open("w") as configwriter:
         json.dump(configfile_contents, configwriter, indent=4)
 
-    app_log.info("Device Calibrated")
+    app_log.info("Device Calibration Complete")
     app_log.info(configfile_contents["device"])
-    print("Device Calibrated")
-
+    print("Device Calibration Complete")
 
 if __name__ == "__main__":
-   calibrate_all()
+   calibrate(True,True,True,True)
--- device_controller/mm_controller/cli.py
+++ device_controller/mm_controller/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import sys
 from importlib.metadata import version
 import subprocess
@@ -51,27 +52,40 @@ def cli():
     parser.add_argument("-s", "--start", action="store_true", help="Start the controller.")
     parser.add_argument("-v", "--version", action="store_true", help="Print version and exit.")
     parser.add_argument("-d", "--diagnostics", action="store_true", help="Run diagnostics.")
-    parser.add_argument("-c", "--calibrate", action="store_true", help="Calibrate connected devices.")
+    parser.add_argument("-c", "--calibrate", choices=["SCALE", "LINE_IN", "LINE_OUT", "VACUUM", "ALL"], default="ALL", help="Calibrate connected devices.")
     parser.add_argument("-r", "--register", action="store_true", help=argparse.SUPPRESS) # TODO secure this so consumers can't register devices
     parser.add_argument("-n", "--clean", action="store_true", help="Remove WiFi and logs from device.")
     parser.add_argument("--validate", choices=["cloud", "weather"], help="Run various exercises to validate connectivity.")
+    parser.add_argument("-u", "--upgrade", action="store_true", help="Upgrade the controller package (considers all whl files in /home/mm)")
     parser.add_argument("-l", "--loglevel", choices=["DEBUG", "INFO", "WARN", "ERROR"], default="DEBUG", help="Set logging level.")
 
     args = parser.parse_args()
 
+    # Print version and exit
     if args.version:
         print(version('mm_controller'))
         sys.exit(0)
     
+    # Calibrate the device
     if args.calibrate:
         try:
             stop_service()
             print("Running calibration...")
             from mm_controller import calibrate_device
-            calibrate_device.calibrate_all()
+            if args.calibrate == "SCALE" or args.calibrate == "ALL":
+                calibrate_device.calibrate(scale=True)
+            if args.calibrate == "LINE_IN" or args.calibrate == "ALL":
+                calibrate_device.calibrate(line_in=True)
+            if args.calibrate == "LINE_OUT" or args.calibrate == "ALL":
+                calibrate_device.calibrate(line_out=True)
+            if args.calibrate == "VACUUM" or args.calibrate == "ALL":
+                calibrate_device.calibrate(vacuum=True)
         except KeyboardInterrupt:
+            print("Calibration aborted.")
+        finally:
             start_service()
-        
+
+    # Clean the device
     if args.clean:
         print("Cleaning device...")
         # from mm_controller import clean_device
@@ -80,6 +94,7 @@ def cli():
         # clean_device.clean()
         # start_service()
 
+    # Register the device
     if args.register:
         print("Running registration...")
         stop_service()
@@ -91,6 +106,7 @@ def cli():
             print("Device password exists. Skipping...")
             start_service()
 
+    # Run diagnostics
     if args.diagnostics:
         try:
             stop_service()
@@ -106,6 +122,32 @@ def cli():
     elif args.validate == "weather":
        pass
 
+    # Upgrade the controller package
+    if args.upgrade:
+        print("Upgrading...")
+        print("Current version: %s" % version('mm_controller'))
+        print("Looking for upgrades...")
+        path = "/home/mm"
+        dir_list = os.listdir(path)
+        for filename in dir_list:
+            match = re.match(r"mm_controller-(.*)-py3-none-any.whl", filename)
+            if match:
+                version_match = match.group(1)
+                if version_match > version('mm_controller'):
+                    try:
+                        print("Upgrading to %s" % version_match)
+                        stop_service()
+                        subprocess.run([".ctrlenv/bin/pip", "install", os.path.join(path, filename)])
+                        print("Upgrade complete.")
+                    except:
+                        print("Upgrade failed.")
+                    finally:
+                        start_service()
+                    sys.exit(0)
+        print("No upgrades found.")
+        sys.exit(0)
+
+    # Start the controller
     if args.start:        
         if not is_instance_running():
             print("Log level: %s" % args.loglevel)
@@ -117,7 +159,10 @@ def cli():
             controller.run()
         else:
             print("Controller is running. Use /usr/bin/systemctl.")
-    sys.exit(0)
+
+    # Print help if nothing else happened yet
+    parser.print_help()
+    parser.exit()
 
 if __name__ == "__main__":
    cli()
